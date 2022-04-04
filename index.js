d4d1efefd27e4e627ebf5d53949b0b0751a21e54
davitelesfranca <davi.teles.franca@gmail.com>
2022-04-03 21:53:25 -0300

    adding a few code comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 
+//In this const, is built the desired output for the challenge
 const getJSONData = (date,remoteIpAdrres)  => ({
   "timestamp": date.toUTCString(),
   "hostname": require('os').hostname(),
@@ -10,14 +11,13 @@ const getJSONData = (date,remoteIpAdrres)  => ({
 
 const requestHandler = (req, res) => {
   if (req.url === "/") { 
+    //Here we can have the visitor IP
     let remoteIpAdrres = req.socket.remoteAddress.slice(7);
     let timestamp = getJSONData(new Date(), remoteIpAdrres);
     
-
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(timestamp));
-   
-     
+        
   } else {
     fs.readFile("views/404.html", (err, html) => {
       if (err) throw err;
